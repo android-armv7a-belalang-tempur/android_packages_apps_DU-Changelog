e0d52ea40bc8bc16dbe2f5b58a479f7f1de7d4ab
mazwoz <mazwoz@gmail.com>
2013-06-08 15:44:05 -0700

    Made buildable inline
    Package now builds inline with the rest of DU

--- Android.mk
+++ Android.mk
@@ -3,15 +3,12 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES += $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under) $(call all-renderscript-files-under, src)
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4
 
-LOCAL_PACKAGE_NAME := DU Changelog
-
-LOCAL_SDK_VERSION := current
-LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_PACKAGE_NAME := DirtyUnicornsChangelog
 
 include $(BUILD_PACKAGE)
 
