ecf01da225d37f712ec135d7727204a87de1b0a1
mazwoz <mazwoz@gmail.com>
2013-06-08 15:23:55 -0700

    Initial Commit

---
+++ build/source/buildConfig/debug/us/zamzow/mazwoz/dirtyunicornschangelog/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package us.zamzow.mazwoz.dirtyunicornschangelog;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
