142288dfdf6be9d73bc6b377e69151fb847df614
Alex <mazdarider23@gmail.com>
2013-12-24 02:58:26 -0600

    Part 1 of integrating into Settings

--- Android.mk
+++ Android.mk
@@ -1,14 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_SRC_FILES += $(call all-java-files-under) $(call all-renderscript-files-under, src)
-
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v4
-
-LOCAL_PACKAGE_NAME := DUChangelog
-
-include $(BUILD_PACKAGE)
+#include $(CLEAR_VARS)
+#
+#LOCAL_MODULE_TAGS := optional
+#
+#LOCAL_SRC_FILES += $(call all-java-files-under) $(call all-renderscript-files-under, src)
+#
+#LOCAL_STATIC_JAVA_LIBRARIES := \
+#    android-support-v4
+#
+#LOCAL_PACKAGE_NAME := DU_changelog
+#
+#include $(BUILD_PACKAGE)
 
